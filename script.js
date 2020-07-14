64a5a6dd710847c7ce83973ca04afcd81fed4408
ViniciusLisboa07 <63267152+ViniciusLisboa07@users.noreply.github.com>
2020-07-14 16:18:43 -0300

    arrumando funções

--- script.js
+++ script.js
@@ -18,19 +18,52 @@ for (let i = 0; i < _boxes.length; i++) {
     // quando alguem clicka na caixa
     _boxes[i].addEventListener("click", function(){
 
-        let el;
+        let el = checkEl(player1, player2);
 
-        if(player1 == player2){
-            // x
-            el = x;
-        } else{
-            // o
-            el = o;
+        
+
+        if(this.childNodes.length == 0){
+
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+
+            // marcando as jogadas
+            if(player1 == player2){
+                player1++;
+            } else {
+                player2++;
+            }
         }
 
-        let cloneEl = el.cloneNode(true);
-
-        this.appendChild(cloneEl);
+        checarCondiçãoVitoria();
     });
     
 }
+
+// verifica quem vai jogar
+function checkEl(player1, player2){
+    if(player1 == player2){
+        // x
+        el = x;
+    } else{
+        // o
+        el = o;
+    }
+
+    return el;
+}
+
+// verifica vitória
+ function checarCondiçãoVitoria(){
+
+    let b1 = document.getElementById("block-1");
+    let b2 = document.getElementById("block-2");
+    let b3 = document.getElementById("block-3");
+    let b4 = document.getElementById("block-4");
+    let b5 = document.getElementById("block-5");
+    let b6 = document.getElementById("block-6");
+    let b7 = document.getElementById("block-7");
+    let b8 = document.getElementById("block-8");
+    let b9 = document.getElementById("block-9");
+ }
